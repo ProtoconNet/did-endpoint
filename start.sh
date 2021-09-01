d1b5de14bc74ba3b10f3fb62cc581214786f7c12
securekim <admin@securekim.com>
2021-09-02 01:03:57 +0900

    Change start.sh for cron

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
-sampleFile="src/configs/samples.py"
-cp $sampleFile ../_samples.py
+DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+cd $DIR
+sampleFile="$DIR/src/configs/samples.py"
+cp $sampleFile $DIR/../_samples.py
 rm $sampleFile
 git pull
 if [ -f "$sampleFile" ]; then
-	echo "file exist"
-	sed -e s/"127.0.0.1"/"mtm.securekim.com"/g $sampleFile > $sampleFile.tmp && mv $sampleFile.tmp $sampleFile
+        echo "file exist"
+        sed -e s/"127.0.0.1"/"mtm.securekim.com"/g $sampleFile > $sampleFile.tmp && mv $sampleFile.tmp $sampleFile
 else
-	echo "file not exist"
-	cp ../_samples.py $sampleFile
+        echo "file not exist"
+        cp $DIR/../_samples.py $sampleFile
 fi
 killall python3
-python3 src/issuer.py&
-python3 src/verifier.py&
-
+python3 $DIR/src/issuer.py&
+python3 $DIR/src/verifier.py&
